04eefcd04cea51284f80ccbe250d5cee1487eaa0
Julianjuyo <je.oliverosf@uniandes.edu.co>
2024-11-05 21:47:05 -0500

    Se crean los modelos

---
+++ app/src/main/java/com/mfpe/vinilos/data/model/Musician.kt
@@ -0,0 +1,14 @@
+package com.mfpe.vinilos.data.model
+import java.io.Serializable
+import java.util.Date
+
+
+data class Musician (
+    val id: Int,
+    val name: String,
+    val image: String,
+    val description: String,
+    val birthDate: Date,
+    val band : Band
+
+): Serializable
